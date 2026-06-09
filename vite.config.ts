d3250dc3c3fdb42ea92f6a58f28bce1d0e7ad785
Daniel Silva Rocha <danielddsilva2@hotmail.com>
2026-06-09 20:35:05 -0300

    fix: build warning.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	define:{
+		'import.meta':{}
+	},
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
