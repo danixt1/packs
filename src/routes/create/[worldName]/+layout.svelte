d1e12be2cf533aacd9065350256c3c21c5180129
Daniel Silva Rocha <danielddsilva2@hotmail.com>
2026-07-05 20:55:28 -0300

    feat:(initial) create pages organization.

---
+++ src/routes/create/[worldName]/+layout.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import TopBar from '$lib/components/TopBar.svelte';
+    import { goto } from '$app/navigation';
+    let { children } = $props();
+    let menus:{display:string,name:string}[] = [
+        {
+            display:'Settings',
+            name:'settings'
+        },
+        {
+            display:'Items',
+            name:'items'
+        },
+        {
+            display:'Characters',
+            name:'characters'
+        },
+        {
+            display:'Locations',
+            name:'locations'
+        },
+        {
+            display:'Events',
+            name:'events'
+        },
+        {
+            display:'Dialogues',
+            name:'dialogues'
+        },
+        {
+            display:'Actions',
+            name:'actions'
+        }
+    ]
+</script>
+
+{@render children()}
---
+++ src/routes/create/[worldName]/+page.svelte
@@ -0,0 +1,24 @@
+<script lang='ts'>
+    import { goto } from "$app/navigation";
+    import type { World } from "$lib/types/data/declarative";
+    import type { PageProps } from './$types';
+    let { data }: PageProps = $props();
+    // svelte-ignore state_referenced_locally
+    let selectedWorld:World = $state(data.world);
+</script>
+
+<div class="world-edit">
+    <div class="upper-bar">
+        <input type="text" name="worldName" id="worldName" value={selectedWorld.name} />
+        <textarea name="worldDescription" id="worldDescription">{selectedWorld.description}</textarea>
+        <button type="button" onclick={()=>{goto('#/create')}}>Back to Worlds</button>
+    </div>
+    <main>
+        <div class="search">
+            <input type="text" name="search" id="search" placeholder="Search for data..." />
+        </div>
+        <div>
+            
+        </div>
+    </main>
+</div>
