65cf0418b3edd111ec980c191362fb1b47c514ea
Daniel Silva Rocha <danielddsilva2@hotmail.com>
2026-07-12 13:02:17 -0300

    feat:(initial) base character creation

---
+++ src/routes/create/[worldName]/characters/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    //TODO: add the data when the editor backend is ready.
+    let tempData = {
+        labels:[
+            {
+                name:'npc',
+                description:'is a Non-player character',
+                usedBy:[
+                    {
+                        type:'dialogue',
+                        mappingId:'dial.small-talk-npc',
+                        id:'small-talk-npc',
+                        name:'NPC Small Talk',
+                        priority:'low' // pre-process priority in backend to determine UI order and colors.
+                    }
+                ]
+            }
+        ],
+        characters:[
+            {
+                id:'char1',
+                name:'Character 1',
+                vars:[
+                    {
+                        id:'desc',
+                        name:'Description',
+                        type:'string',
+                        value:'The first character in the world'
+                    },
+                    {
+                        id:'health',
+                        name:'Health',
+                        type:'number',
+                        min:0,
+                        max:100,
+                        value:100
+                    }
+                ]
+            }
+        ]
+    }
+</script>
